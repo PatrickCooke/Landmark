511d6b4d3cdcdd034e3fd7ea895e0144ce9f5899
PatrickCooke <cookepa1@gmail.com>
2016-05-02 12:06:47 -0400

    Initial Commit

---
+++ Landmark/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Landmark
+//
+//  Created by Patrick Cooke on 5/2/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Landmark/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Landmark
+//
+//  Created by Patrick Cooke on 5/2/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
